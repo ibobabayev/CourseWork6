89dcf8cd77783fc8f70c3f961e1613dd5dd821ab
user <ibrahim42babayev@gmail.com>
2024-04-16 14:01:06 +0300

    #3 try

--- config/settings.py
+++ config/settings.py
@@ -16,18 +16,21 @@ from dotenv import load_dotenv
 import os
 
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+pszxi0vltk34@kifa5dp)t5%0d0*kgek2$_k4r8gshwrcbc49'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -84,9 +87,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangocw',
-        'USER': 'postgres',
-        'HOST': '127.0.0.1',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'HOST': os.getenv('HOST'),
         'PORT': 5432
     }
 }
@@ -137,7 +140,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-load_dotenv(BASE_DIR / '.env')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.mail.ru'
